46c10af4925136de27feb554fe2abac267b7d46a
Piero Viscone <pieroviscone@gmail.com>
2025-09-02 00:00:24 +0200

    First (and hopefully last) commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Just run
+
+```bash
+./setup.sh 2024 #or 2022 or 2023 (2022 and 2023 are the same)
+```
+
+It will create a CMSSW release the repo folder with everything in place to create Xee ntuples.
---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+export XEE_PATH=`dirname "$(realpath $0)"`
+export XEE_EXTERNALS=$XEE_PATH/externals
+
+if [ "$1" == "2024" ]; then
+    export XEE_RELEASE="CMSSW_15_0_2";
+elif [ "$1" == "2023" ]; then
+    export XEE_RELEASE="CMSSW_13_0_13"
+elif [ "$1" == "2022" ]; then
+    export XEE_RELEASE="CMSSW_13_0_13"
+else
+    echo "ERA $1 not implemented"
+    exit 1;
+fi
+
+#cmsrel
+scramv1 project $XEE_RELEASE
+export CMSSW_SRC=$XEE_PATH/$XEE_RELEASE/src
+
+pushd $CMSSW_SRC
+eval `scramv1 runtime -sh` #cmsenv
+git cms-init
+
+git cms-addpkg PhysicsTools/PatAlgos
+git cms-addpkg TrackingTools/TransientTrack
+git cms-addpkg RecoVertex/KinematicFit
+git cms-addpkg RecoVertex/VertexTools
+
+git clone git@github.com:pviscone/DoubleElectronNANO.git
+git clone -b dpee git@github.com:pviscone/EgammaPostRecoTools.git EgammaUser/EgammaPostRecoTools
+
+cp -rf $XEE_EXTERNALS/* $CMSSW_SRC/
+
+read -p "Do you want to scram now? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    scram b -j 8
+fi
